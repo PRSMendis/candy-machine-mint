679a791ac36f1a8bf9276c6a6428a8355345e464
Rayhan Mendis <rayhanon@gmail.com>
2021-11-13 19:29:51 +1100

    logic should be right

--- src/components/Cdown.tsx
+++ src/components/Cdown.tsx
@@ -1,40 +1,61 @@
 import * as React from "react"; 
 import { useEffect, useState } from "react";
 import './Cdown.css'
+import moment from 'moment';
 
-function Cdown() {
-    const [seconds, setSeconds] = useState(0);
+
+
+
+
+const Cdown = () =>  {
+    const [seconds, setSeconds] = useState(1);
     const [minutes, setMinutes] = useState(0);
     const [hours, setHours] = useState(0);
     const [days, setDays] = useState(0);
+    const timeTillDate = "12 05 2021, 1:00 am"
+    const timeFormat = "MM DD YYYY, h:mm a"
 
-    let interval = useRef();
+    useEffect(() => {
+        console.log('render')
+        const timer = setTimeout(() => {
+            const then = moment(timeTillDate, timeFormat);
+            const now = moment();
+            const countdown = moment(then.valueOf() - now.valueOf());
+            const days = countdown.format('D');
+            const hours = countdown.format('HH');
+            const minutes = countdown.format('mm');
+            const seconds = countdown.format('ss');
 
-    const startTimer = () => {
-        const countdown_date = new Date('December 05, 2021, 00:00:00').getTime(); 
+          }, 1000);
 
-        interval = setInterval()
-    }
+    }, [])
+
+    
 
 
     return (
-        <div className="container">
-            <h1 id="headline">Countdown to my birthday</h1>
-            <div id="countdown">
-                <ul>
-                <li><span id="days">{seconds}</span>days</li>
-                <li><span id="hours"></span>Hours</li>
-                <li><span id="minutes"></span>Minutes</li>
-                <li><span id="seconds"></span>Seconds</li>
-                </ul>
-            </div>
-            <div id="content" className="emoji">
-                <span>🥳</span>
-                <span>🎉</span>
-                <span>🎂</span>
+        <div>
+            <h1>Countdown</h1>
+            <div className="countdown-wrapper">
+                <div className="countdown-item">
+                    {days}
+                    <span>days</span>
+                </div>
+                <div className="countdown-item">
+                    {hours}
+                    <span>hours</span>
+                </div>
+                <div className="countdown-item">
+                    {minutes}
+                    <span>minutes</span>
+                </div>
+                <div className="countdown-item">
+                    {seconds}
+                    <span>seconds</span>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Cdown;
