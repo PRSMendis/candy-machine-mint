3d13b884619b4f1dd7824aeae38190f59dc0cd78
Rayhan Mendis <rayhanon@gmail.com>
2021-11-14 12:38:32 +1100

    styled countdown

--- src/components/Cdown.tsx
+++ src/components/Cdown.tsx
@@ -33,7 +33,7 @@ const Cdown = () =>  {
 
     return (
         <div>
-            <h1>Countdown</h1>
+            <h1>Time Til Mint</h1>
             <div className="countdown-wrapper">
                 <div className="countdown-item">
                     {days}
